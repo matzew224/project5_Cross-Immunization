4da6a86d43e5b4eedb65624479adbad1119dfb65
mathiasw224 <mahiasw224@zedta.fu-berlin.de>
2024-09-27 18:03:06 +0200

    code cleanup

--- download_and_plot_outbreak_data.R
+++ download_and_plot_outbreak_data.R
@@ -9,12 +9,12 @@ make_sure_dir_exists <- function(dir_path) {
 }
 
 extract_mutation_only <- function(mutation_table){
-  # Check if the "mutation_count" column exists
-  if (!"mutation_count" %in% colnames(mutation_table)) {
-    stop("The column 'mutation_count' is not found in the DataFrame.")
+  # Check if the "mutation" column exists
+  if (!"mutation" %in% colnames(mutation_table)) {
+    stop("The column 'mutation' is not found in the DataFrame.")
   }
   
-  # Strip "s:" from the beginning of each string in the "mutation_count" column
+  # Strip "s:" from the beginning of each string in the "mutation" column
   mutation_table$mutation_count <- gsub("^s:", "", mutation_table$mutation)
   
   # Return the cleaned "mutation_count" column
@@ -27,16 +27,20 @@ download_mutation_profiles <- function(lineages, output_dir){
   downloaded_paths = c()
   
   for (lineage in lineages) {
-    mutations = getMutationsByLineage(pangolin_lineage=lineage , frequency=0.75, logInfo = FALSE)
+    mutations = getMutationsByLineage(pangolin_lineage=lineage , frequency=0.75,
+                                      logInfo = FALSE)
     
     # filter for Spike:
     mutations_s <- subset(mutations, gene=="S")
     mutations_s_only_mutation <- extract_mutation_only(mutations_s)
     # save to file
-    filepath_raw = paste(paste(output_dir,"/mutation", sep=""), lineage, ".txt", sep="_")
-    filepath_stripped = paste(paste(output_dir,"/mutation", sep=""), lineage, "_stripped.txt", sep="_")
+    filepath_raw = paste(paste(output_dir,"/mutation", sep=""), 
+                         lineage, ".txt", sep="_")
+    filepath_stripped = paste(paste(output_dir,"/mutation", sep=""), 
+                              lineage, "_stripped.txt", sep="_")
     write.table(mutations_s, file=filepath_raw)
-    write.table(mutations_s_only_mutation, file=filepath_stripped, row.names = FALSE, col.names = FALSE, quote=FALSE)
+    write.table(mutations_s_only_mutation, file=filepath_stripped, 
+                row.names = FALSE, col.names = FALSE, quote=FALSE)
     downloaded_paths_raw = c(downloaded_paths, filepath_raw)
   }
   return(downloaded_paths_raw)
@@ -46,14 +50,18 @@ plot_mutation_profiles <- function(muation_profile_paths, out_dir){
   make_sure_dir_exists(out_dir)
   for (profile_path in mutation_profile_paths){
     file_basename <- basename(profile_path)
-    file_basename_no_ext = str_sub(tools::file_path_sans_ext(file_basename), end=-2)
+    file_basename_no_ext = str_sub(tools::file_path_sans_ext(file_basename), 
+                                   end=-2)
     output_filename = paste("heatmap_", file_basename_no_ext, ".png", sep="")
     output_filepath= paste(out_dir, output_filename, sep="/")
     # Plot the mutations as a heatmap and save it
     print(paste("Saving plot at: ", output_filepath))
     mutation = read.table(profile_path)
-    this_plot <- plotMutationHeatmap(mutation, title = paste("S-gene mutations in lineage", gsub("mutation_", "",file_basename_no_ext), sep=" "))
-    ggplot2::ggsave(filename = output_filepath, plot = this_plot, width = 10, height = 6, dpi = 300)
+    this_plot <- plotMutationHeatmap(mutation, 
+                          title = paste("S-gene mutations in lineage",
+                          gsub("mutation_", "",file_basename_no_ext), sep=" "))
+    ggplot2::ggsave(filename = output_filepath, plot = this_plot, 
+                    width = 10, height = 6, dpi = 300)
   }
 }
 
